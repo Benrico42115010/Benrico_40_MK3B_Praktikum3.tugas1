7f52eacf91169827752919c5cf34c573ed4d1f33
Benrico42115010 <benricosimamora@gmail.com>
2016-09-13 10:12:35 +0700

    Update tugas1.ino

--- tugas1.ino
+++ tugas1.ino
@@ -1,45 +1,50 @@
-void setup() 
-{
+void setup() {
   // put your setup code here, to run once:
-DDRB=0b1111;
-DDRC=0b0000;
-PORTC=0b1111;
-testled();
+  DDRB = 15;
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+  PORTB=0;
 }
 
-void loop() 
-{
+void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(A0)==LOW)
-    {
-  //counterUP();
-   PORTB = PORTB + 1;
-   delay(1000);
-    }
-  //counterdown
-  if(digitalRead(A1)==LOW)
-    {
-      PORTB = PORTB - 1;
-      delay(1000);
-    }
-  //reset
-  if(digitalRead(A2)==LOW)
-    {
-      PORTB=0;
-    }
+  if (digitalRead(A0) == LOW)
+  {
+  ledup();
+  }
+  else if (digitalRead(A1) == LOW)
+  {
+  leddown();
+  }
+  else if (digitalRead(A2) == LOW)
+  {
+    PORTB = 0;
+  }
 }
-void testled()
-{
-PORTB=0b1111;
-delay(100);
-PORTB=0b000;
-delay(100);
-PORTB=0b1111;
-delay(100);
-PORTB=0b000;
-delay(100);
-PORTB=0b1111;
-delay(100);
-PORTB=0b000;
-delay(100);
+void ledup(){
+  for (int i = 0; i < 64; i++)
+  {
+    PORTB = PORTB +1;
+    delay(1000);  
+   if (digitalRead(A1) == LOW)
+  {
+  break;
+
+  }
+  }
+}
+void leddown(){
+  for (int i = 64; i >= 0; i--)
+  {
+    PORTB = PORTB-1 ;
+    delay (1000);
+   if (digitalRead(A0) == LOW)
+  {
+  break;
+  }
+  }
 }
